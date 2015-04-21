ca53a92d40621c40252a1fa1651bcaa7438197bd
Norawit Urailertprasert <wit543@gamil.com>
2015-04-21 12:52:17 +0700

    move the function is static to Function

--- GCG/src/Function.js
+++ GCG/src/Function.js
@@ -16,3 +16,55 @@ var GetBottomY = function(){
 var GetLeftX = function(){
     return cc.rectGetMinX(this.getBoundingBoxToWorld());
 };
+
+var HitTop= function(oldRect, newRect){
+    var brect = this.getBoundingBoxToWorld();
+
+    if(cc.rectGetMinY(oldRect)>= cc.rectGetMaxY(brect)){
+        var loweredNewRect = cc.rect(newRect.x,newRect.y-1,newRect.width,newRect.height+1);
+        var uRect = cc.rectUnion(oldRect,loweredNewRect);
+        var newBrect = cc.rect(brect.x+1,brect.y,brect.width-2,brect.height);
+        return cc.rectIntersectsRect(uRect, newBrect);
+    }
+    return false;
+};
+
+var HitBottom=function(oldRect,newRect){
+    var brect = this.getBoundingBoxToWorld();
+
+    if(cc.rectGetMaxY(oldRect)>= cc.rectGetMinY(brect)){
+        var upedNewRect = cc.rect(oldRect.x,newRect.y,newRect.width,newRect.height+1);
+        var uRect = cc.rectUnion(oldRect,upedNewRect);
+        var newBrect = cc.rect(brect.x+1,brect.y,brect.width-2,brect.height-1);
+        if(cc.rectIntersectsRect(uRect, newBrect)){
+            //console.log(oldRect);
+            //console.log(newRect);
+            //console.log(uRect);
+            //console.log(newBrect);
+            return true;
+        }
+    }
+    return false;
+};
+
+var HitRight=function(oldRect,newRect){
+    var brect = this.getBoundingBoxToWorld();
+    if(oldRect.x>=brect.x&&oldRect.x<=brect.x+50){
+        if(oldRect.y>brect.y-50&&oldRect.y<brect.y+50) {
+            //console.log(oldRect.x+" "+brect.x);
+            return true;
+        }
+    }
+    return false;
+}
+
+var HitLeft=function(oldRect,newRect){
+    var brect = this.getBoundingBoxToWorld();
+    if(oldRect.x+50>=brect.x&&oldRect.x+50<=brect.x+50){
+        if(oldRect.y>brect.y-50&&oldRect.y<brect.y+50) {
+            // console.log(oldRect.x-brect.x);
+            return true;
+        }
+    }
+    return false;
+};
